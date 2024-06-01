cb66318684764f11562ca4c9ba4b03eb1b4ad7c4
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-06-01 08:35:34 -0300

    day #20 complete

---
+++ challenges/day_020/Day020.java
@@ -0,0 +1,8 @@
+public class Day020 {
+
+   public static void main(String[] args) {
+    GerenciamentoProdutos gpt = new GerenciamentoProdutos();
+    gpt.adicionarProduto("Arroz Branco", 20);
+    gpt.ListarProdutos();
+   }
+}
---
+++ challenges/day_020/README.md
@@ -0,0 +1,10 @@
+# Day 20
+
+## Desafio:
+
+	Crie uma classe em Java para representar um sistema de gerenciamento de produtos com operações de adicionar, remover e atualizar informações dos produtos
+
+**Resultado:**
+
+```java
+
