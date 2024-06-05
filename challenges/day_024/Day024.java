e62166be94c8735d6a37669dd33ddf44a56c3868
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-06-05 16:56:03 -0300

    day #24 complete

---
+++ challenges/day_024/Day024.java
@@ -0,0 +1,37 @@
+
+public class Day024 {
+
+public static int binarySearch(int[] array, int target) {
+  int left = 0;
+  int right = array.length - 1;
+
+  while (left <= right) {
+      int mid = left + (right - left) / 2;
+
+      if (array[mid] == target) {
+          return mid;
+      }
+
+      if (array[mid] < target) {
+          left = mid + 1;
+      } 
+      else {
+          right = mid - 1;
+      }
+  }
+
+  return -1;
+}
+
+public static void main(String[] args) {
+  int[] sortedArray = {2, 3, 4, 10, 40};
+  int target = 10;
+
+  int result = binarySearch(sortedArray, target);
+  if (result == -1) {
+      System.out.println("Elemento não encontrado no array.");
+  } else {
+      System.out.println("Elemento encontrado no índice: " + result);
+  }
+}
+}
