84a527ee1f270e2acf430167e81e28fa465619a1
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-06-28 14:33:05 -0300

    day #47 complete

---
+++ challenges/day_047/Day047.java
@@ -0,0 +1,44 @@
+public class Day047 {
+
+    public static void transitiveClosure(int[][] graph) {
+        int V = graph.length;
+        int[][] reach = new int[V][V];
+
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                reach[i][j] = graph[i][j];
+            }
+        }
+
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    reach[i][j] = (reach[i][j] != 0) || ((reach[i][k] != 0) && (reach[k][j] != 0)) ? 1 : 0;
+                }
+            }
+        }
+
+        printMatrix(reach);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        int V = matrix.length;
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {
+            {0, 1, 0, 0},
+            {0, 0, 1, 0},
+            {1, 0, 0, 1},
+            {0, 0, 0, 0}
+        };
+
+        transitiveClosure(graph);
+    }
+}
