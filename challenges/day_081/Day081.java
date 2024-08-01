fb3a3d28ec22be6bea09b7c2358324ff4ad80f56
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-08-01 10:57:33 -0300

    day #81 complete

---
+++ challenges/day_081/Day081.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day081 {
+
+    public static class TarefasPessoas {
+        List<String> tarefa;
+        List<String> lista;
+        List<Integer> tempo;
+
+        public TarefasPessoas() {
+            this.tarefa = new ArrayList<>();
+            this.lista = new ArrayList<>();
+            this.tempo = new ArrayList<>();
+        }
+
+        public void criarListaTarefas(String tarefas, String listas) {
+            System.out.println("Foi criado sua lista: " + listas);
+            System.out.println("Foi criado sua tarefa: " + tarefas);
+            tarefa.add(tarefas);
+            lista.add(listas);
+        }
+
+        public void prioridadeTarefa(String mensagem, String tarefas) {
+            System.out.println("Sua prioridade é " + mensagem + " para essa tarefa: " + tarefas);
+        }
+
+        public void acompanharProgresso(String tarefas, int tempo) {
+            System.out.println("Sua tarefa " + tarefas + " está sendo realizada e o tempo é " + tempo + " minutos.");
+        }
+
+        public static void main(String[] args) {
+            TarefasPessoas tp = new TarefasPessoas();
+
+            tp.criarListaTarefas("Estudar Java", "Estudos");
+            tp.criarListaTarefas("Comprar pão", "Compras");
+            tp.prioridadeTarefa("Alta", "Estudar Java");
+            tp.acompanharProgresso("Estudar Java", 60);
+            tp.acompanharProgresso("Comprar pão", 15);
+        }
+    }
+}
